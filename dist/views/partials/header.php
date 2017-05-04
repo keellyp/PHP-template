e59d7b446ee0938ede3299e1d20f2ed18554b098
Kelly Phan <keelly.p@gmail.com>
2017-05-04 15:54:36 +0200

    Fix bug

--- dist/views/partials/header.php
+++ dist/views/partials/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Mon template</title>
-        <link rel="stylesheet" href="/dist/css/style.min.css">
+        <link rel="stylesheet" href="dist/css/style.min.css">
     </head>
     <body>
 
